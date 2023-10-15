4124bfabfa50ee42f9b1305c320d9a37cc3e0cb0
berón <bleink@outlook.es>
2023-10-15 19:19:28 +0200

    add modal to big tablets and small laptops

--- src/app/components/styled/template/gallery/gallery-component.ts
+++ src/app/components/styled/template/gallery/gallery-component.ts
@@ -1,6 +1,7 @@
 import { TemplateRef } from "@angular/core";
 import { ColumnOrRowProjection } from "../../molecule/column-or-row/class";
 import { GalleryImages } from "./db/gallery.images";
+import { GalleryImage } from "../../molecule/gallery-image/class";
 
 
 
@@ -22,4 +23,108 @@ export class GalleryComponent {
     this.musicDownProjectionObj.template = pTemplate
     return this.musicDownProjectionObj
   }
+
+
+
+
+  isLimitedToOneModal = true
+
+  imagesGroup:imagesGroupType = {
+    first: [],
+    second: [],
+    third: [],
+    fourth: [],
+    fifth: [],
+    sixth: [],
+    seventh: [],
+    eighth: [],
+    nineth: [],
+    tenth: [],
+    eleventh: [],
+    twelfth: [],
+    thirteenth: [],
+    fourteenth: [],
+    fifteenth: [],
+    sixteenth: [],
+    seventeenth: [],
+    eighteenth: [],
+  }
+
+  handler_toggleModalNumberLimitation:Function = (pImage:GalleryImage):void => {
+    this.isLimitedToOneModal = !this.isLimitedToOneModal;
+    this.removeAllModals();
+    pImage.handlerClick();
+  }
+
+  getImageWithClickHandler = (pImage:GalleryImage, pGroupName:groupNameType) => {
+    pImage.changeHandlerClick(this.toggleToImagesGroupHandler(pImage, pGroupName))
+    return pImage;
+  }
+
+  toggleToImagesGroupHandler = (pImage:GalleryImage, pGroupName:groupNameType) => () => this.toggleToImagesGroup(pImage, pGroupName)
+
+  toggleToImagesGroup = (pImage:GalleryImage, pGroupName:groupNameType):void => {
+    const imageIndexInGroup = this.imagesGroup[pGroupName].indexOf(pImage)
+    if (imageIndexInGroup >= 0) {
+      this.isLimitedToOneModal ? this.removeAllModals() : this.imagesGroup[pGroupName].splice(imageIndexInGroup, 1)
+    } else {
+      if (this.isLimitedToOneModal) this.removeAllModals()
+      this.imagesGroup[pGroupName].push(pImage)
+    }
+  }
+
+  removeAllModals() {
+    this.imagesGroup = {
+      first: [],
+      second: [],
+      third: [],
+      fourth: [],
+      fifth: [],
+      sixth: [],
+      seventh: [],
+      eighth: [],
+      nineth: [],
+      tenth: [],
+      eleventh: [],
+      twelfth: [],
+      thirteenth: [],
+      fourteenth: [],
+      fifteenth: [],
+      sixteenth: [],
+      seventeenth: [],
+      eighteenth: [],
+    }
+  }
+
+
+
+}
+
+
+
+
+
+
+type groupNameType =     'first'|'second'|'third'|'fourth'|'fifth'|'sixth'|'seventh'|'eighth'|'nineth'|'tenth'|'eleventh'|'twelfth'|'thirteenth'|'fourteenth'|'fifteenth'|'sixteenth'|'seventeenth'|'eighteenth'
+
+
+type imagesGroupType = {
+  first: GalleryImage[],
+  second: GalleryImage[],
+  third: GalleryImage[],
+  fourth: GalleryImage[],
+  fifth: GalleryImage[],
+  sixth: GalleryImage[],
+  seventh: GalleryImage[],
+  eighth: GalleryImage[],
+  nineth: GalleryImage[],
+  tenth: GalleryImage[],
+  eleventh: GalleryImage[],
+  twelfth: GalleryImage[],
+  thirteenth: GalleryImage[],
+  fourteenth: GalleryImage[],
+  fifteenth: GalleryImage[],
+  sixteenth: GalleryImage[],
+  seventeenth: GalleryImage[],
+  eighteenth: GalleryImage[]
 }
--- src/app/components/styled/template/gallery/gallery-desktop/gallery-desktop.component.ts
+++ src/app/components/styled/template/gallery/gallery-desktop/gallery-desktop.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { GalleryComponent } from '../gallery-component';
-import { GalleryImage } from '../../../molecule/gallery-image/class';
 
 @Component({
   selector: 'app-gallery-desktop',
@@ -8,98 +7,6 @@ import { GalleryImage } from '../../../molecule/gallery-image/class';
   styleUrls: ['./gallery-desktop.component.scss']
 })
 export class GalleryDesktopComponent extends GalleryComponent {
-
-  isLimitedToOneModal = true
-
-  imagesGroup:imagesGroupType = {
-    first: [],
-    second: [],
-    third: [],
-    fourth: [],
-    fifth: [],
-    sixth: [],
-    seventh: [],
-    eighth: [],
-    nineth: [],
-    tenth: [],
-    eleventh: [],
-    twelfth: [],
-    thirteenth: [],
-    fourteenth: [],
-    fifteenth: [],
-    sixteenth: [],
-    seventeenth: [],
-    eighteenth: [],
-  }
-
-  handler_toggleModalNumberLimitation:Function = (pImage:GalleryImage):void => {
-    this.isLimitedToOneModal = !this.isLimitedToOneModal;
-    this.removeAllModals();
-    pImage.handlerClick();
-  }
-
-  getImageWithClickHandler = (pImage:GalleryImage, pGroupName:groupNameType) => {
-    pImage.changeHandlerClick(this.toggleToImagesGroupHandler(pImage, pGroupName))
-    return pImage;
-  }
-
-  toggleToImagesGroupHandler = (pImage:GalleryImage, pGroupName:groupNameType) => () => this.toggleToImagesGroup(pImage, pGroupName)
-
-  toggleToImagesGroup = (pImage:GalleryImage, pGroupName:groupNameType):void => {
-    const imageIndexInGroup = this.imagesGroup[pGroupName].indexOf(pImage)
-    if (imageIndexInGroup >= 0) {
-      this.isLimitedToOneModal ? this.removeAllModals() : this.imagesGroup[pGroupName].splice(imageIndexInGroup, 1)
-    } else {
-      if (this.isLimitedToOneModal) this.removeAllModals()
-      this.imagesGroup[pGroupName].push(pImage)
-    }
-  }
-
-  removeAllModals() {
-    this.imagesGroup = {
-      first: [],
-      second: [],
-      third: [],
-      fourth: [],
-      fifth: [],
-      sixth: [],
-      seventh: [],
-      eighth: [],
-      nineth: [],
-      tenth: [],
-      eleventh: [],
-      twelfth: [],
-      thirteenth: [],
-      fourteenth: [],
-      fifteenth: [],
-      sixteenth: [],
-      seventeenth: [],
-      eighteenth: [],
-    }
-  }
-  
+ 
 }
 
-type groupNameType =     'first'|'second'|'third'|'fourth'|'fifth'|'sixth'|'seventh'|'eighth'|'nineth'|'tenth'|'eleventh'|'twelfth'|'thirteenth'|'fourteenth'|'fifteenth'|'sixteenth'|'seventeenth'|'eighteenth'
-
-
-type imagesGroupType = {
-  first: GalleryImage[],
-  second: GalleryImage[],
-  third: GalleryImage[],
-  fourth: GalleryImage[],
-  fifth: GalleryImage[],
-  sixth: GalleryImage[],
-  seventh: GalleryImage[],
-  eighth: GalleryImage[],
-  nineth: GalleryImage[],
-  tenth: GalleryImage[],
-  eleventh: GalleryImage[],
-  twelfth: GalleryImage[],
-  thirteenth: GalleryImage[],
-  fourteenth: GalleryImage[],
-  fifteenth: GalleryImage[],
-  sixteenth: GalleryImage[],
-  seventeenth: GalleryImage[],
-  eighteenth: GalleryImage[]
-}
